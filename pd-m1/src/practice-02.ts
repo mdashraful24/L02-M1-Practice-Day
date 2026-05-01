f6dd123b58f2537a77d7a1797e71216ec8adbe09
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-02 01:53:34 +0600

    practice non-primitive data types and understanding concept

--- pd-m1/src/practice-02.ts
+++ pd-m1/src/practice-02.ts
@@ -1,4 +1,4 @@
-// Practice for Primitive and Non-Primitive Types
+// ! Practice for Primitive and Non-Primitive Types
 
 // JavaScript types: string, number, boolean, undefined, null.
 
@@ -11,17 +11,19 @@
 // JavaScript is dynamically type language. thats why we can change any data, any value javascript don't mind.
 
 
+// * Primitive
+
 let userName: string = "Ashraful Islam Ratul";
 
 userName = "Ratul Man";
 userName = 233;
 
-let userId = 123;
+let userId: number = 123;
 
 userId = 567;
 userId = "adfasdfk";
 
-let isAdmin = true;
+let isAdmin: boolean = true;
 
 isAdmin = false;
 isAdmin = 123;
@@ -35,3 +37,62 @@ x = 123;
 y = 123;
 z = 123;
 r = 123;
+
+
+
+// * Non-Primitive
+
+// array, object
+
+let bazarList: string[] = ["sop", "cover", "food"];
+
+bazarList.push("grocery");
+
+
+let mixArr: (string | number)[] = ["montu", 20, "jhontu", 30, "bantu", 40];
+
+mixArr.push(50);
+mixArr.push("mintu");
+mixArr.push(false);
+
+// ? Tuple
+let coordinates: [number, number] = [10, 20];
+let coordinates2: [number, number] = [10, 20, 30];
+
+let friends: [string, string] = ["Piash Islam", "Miad khan"];
+
+
+// reference type: object
+
+const user: {
+    readonly userId: number; // * access modifier
+    organization?: string;
+    firstName: string;
+    middleName?: string; // * optional type
+    lastName: string;
+    role: "Student"; // * value => type (Literal Type)
+    otherInfo: {
+        age: number;
+        gender: string;
+        status: string;
+        isMarried: boolean;
+    }
+} = {
+    userId: 22150076,
+    organization: "Ma Baba-r Sontan",
+    firstName: "Ashraful",
+    middleName: "Islam",
+    lastName: "Ratul",
+    role: "Student",
+    otherInfo: {
+        age: 25,
+        gender: "male",
+        status: "Bachelor",
+        isMarried: false
+    }
+}
+
+user.role = "Ma Baba-r Chele";
+user.userId = 1234;
+
+console.log(user);
