a2314facc948357c95fd1eddf55775ff9c73c626
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-04 19:07:18 +0600

    practice another way

--- pd-m2/practice-07.ts
+++ pd-m2/practice-07.ts
@@ -1,14 +1,14 @@
 const UserRoles = {
-    admin: "admin",
-    editor: "editor",
+    admin: "Admin",
+    editor: "EDITOR",
     viewer: "viewer"
 } as const;
 
-const canEdit = (role: keyof typeof UserRoles) => {
+const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
     if (role === UserRoles.admin || role === UserRoles.editor) {
         return true;
     } else return false;
 };
 
-const result = canEdit("admin");
+const result = canEdit("Admin");
 console.log(result);
