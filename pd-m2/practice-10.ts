b11af899274cd3375b522e900d56084ea8e9b024
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-04 19:43:08 +0600

    utility types and full module practice done

---
+++ pd-m2/practice-10.ts
@@ -0,0 +1,42 @@
+// ! Utility Types
+
+// Pick
+// Omit
+// Required
+// Partial
+// Readonly
+// Record
+
+interface Product {
+    id: string;
+    name: string;
+    price: string;
+    stock: string;
+    color?: string;
+};
+
+type ProductSummary = Pick<Product, "id" | "name" | "price">;
+
+type ProductWithOutStock = Omit<Product, "stock" | "color">;
+
+type ProductWithColor = Required<Product>;
+
+const product: ProductWithColor = {
+    id: "123",
+    name: "Code",
+    price: "1000",
+    stock: "5",
+    color: "White"
+};
+
+type OptionalProduct = Partial<Product>;
+
+type ProductReadOnly = Readonly<Product>;
+
+const emptyObj: Record<string, unknown> = {};
+
+const product1 = {
+    id: "123",
+    name: "Code",
+    price: "1000"
+}
