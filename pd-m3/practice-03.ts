cc0607240ba26dbd99240520a6be518fe190d610
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-06 02:00:38 +0600

    instanceof and function guard or narrowing practice done

--- pd-m3/practice-03.ts
+++ pd-m3/practice-03.ts
@@ -1,4 +1,6 @@
-// ! Type guard using typeof and in
+// ! Type guard using typeof, in, instanceof and function
+
+// * typeof guard
 
 type Alphanumeric = number | string;
 
@@ -10,11 +12,13 @@ const add = (num1: Alphanumeric, num2: Alphanumeric) => {
     }
 };
 
-console.log(add(2, 2));
-console.log(add("2", 2));
-console.log(add(2, "2"));
-console.log(add("2", "2"));
+// console.log(add(2, 2));
+// console.log(add("2", 2));
+// console.log(add(2, "2"));
+// console.log(add("2", "2"));
 
+
+// * in guard
 
 type NormalUser = {
     name: string;
@@ -27,8 +31,78 @@ type AdminUser = {
 
 const getUserInfo = (user: NormalUser | AdminUser) => {
     if ("role" in user) {
-        console.log(`${user.name} and his role is ${user.role}`);
-    } else console.log(`${user.name}`);
+        // console.log(`${user.name} and his role is ${user.role}`);
+    } else {
+        // console.log(`${user.name}`);
+    }
 };
 
 getUserInfo({ name: "Normal", role: "Admin" });
+
+
+// * instanceof guard
+
+class PersonGuard {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    };
+
+    getSleep(numOfHours: number) {
+        console.log(`${this.name} doinik ${numOfHours} ghonta ghumai.`);
+    };
+};
+
+class Student1 extends PersonGuard {
+    constructor(name: string) {
+        super(name);
+    };
+
+    doStudy(numOfHours: number) {
+        console.log(`${this.name} doinik ${numOfHours} ghonta study kore.`);
+    };
+};
+
+class Teacher1 extends PersonGuard {
+    designation: string;
+
+    constructor(name: string, designation: string) {
+        super(name);
+        this.designation = designation;
+    }
+
+    takeClass(numOfHours: number) {
+        console.log(`${this.name} designaton ${this.designation} doinik ${numOfHours} ghonta class nei.`)
+    };
+};
+
+
+// function guard
+
+const isStudent = (user: PersonGuard) => {
+    return user instanceof Student1;
+};
+
+const isTeacher = (user: PersonGuard) => {
+    return user instanceof Teacher1;
+};
+
+const getUserInfo2 = (user: PersonGuard) => {
+    if (isStudent(user)) {
+        user.doStudy(10);
+    } else if (isTeacher(user)) {
+        user.takeClass(12);
+    } else {
+        user.getSleep(6);
+    }
+};
+
+const student1 = new Student1("Mr. Student");
+const teacher1 = new Teacher1("Mr. Teacher", "Senior Lecturer");
+const person1 = new PersonGuard("Mr. Fakibaz");
+
+getUserInfo2(person1);
+
+
+// Only humans make mistakes.
