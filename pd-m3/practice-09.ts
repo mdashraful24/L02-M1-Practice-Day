99bffbee031e58a030e0f5366dd87abf46e6a55e
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-06 03:35:55 +0600

    encapsulation concepts practice done

---
+++ pd-m3/practice-09.ts
@@ -0,0 +1,35 @@
+// ! Encapsulation, the 4th pillar of OOP
+
+class BankAccount {
+    readonly userId: number;
+    userName: string;
+    private userBalance: number;
+    protected userAccountNo: string;
+
+    constructor(userId: number,
+        userName: string,
+        userBalance: number, userAccountNo: string) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+        this.userAccountNo = userAccountNo;
+    };
+
+    private addBalance(numOfAmount: number) {
+        this.userBalance = this.userBalance + numOfAmount;
+    }
+
+    callHiddenMethod(numOfAmount: number) {
+        this.addBalance(numOfAmount);
+    }
+};
+
+class StudentAccountInfo extends BankAccount {
+    studentAccount() {
+        this.userAccountNo;
+    }
+}
+
+const myAccount = new BankAccount(111, "Mr. Hasan", 50000, "AC/12378945674");
+
+myAccount.callHiddenMethod(10);
