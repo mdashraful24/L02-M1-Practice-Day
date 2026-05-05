584627f55d2a0382dbb5b854189cade5cc5e244e
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-06 02:17:37 +0600

    access modifier practice done

--- pd-m3/practice-04.ts
+++ pd-m3/practice-04.ts
@@ -4,13 +4,15 @@ class BankAccount {
     readonly userId: number;
     userName: string;
     private userBalance: number;
+    protected userAccountNo: string;
 
     constructor(userId: number,
         userName: string,
-        userBalance: number) {
+        userBalance: number, userAccountNo: string) {
         this.userId = userId;
         this.userName = userName;
         this.userBalance = userBalance;
+        this.userAccountNo = userAccountNo;
     };
 
     addBalance(numOfAmount: number) {
@@ -18,7 +20,13 @@ class BankAccount {
     }
 };
 
-const myAccount = new BankAccount(111, "Mr. Hasan", 50000);
+class StudentAccountInfo extends BankAccount {
+    studentAccount() {
+        this.userAccountNo;
+    }
+}
+
+const myAccount = new BankAccount(111, "Mr. Hasan", 50000, "AC/12378945674");
 
 myAccount.addBalance(50);
 
